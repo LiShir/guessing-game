49a6216a59835d84c6c1d26d6b2a942d0e2c792b
Lizaveta  Shirokova <vardomskaja@rambler.ru>
2017-09-24 19:17:19 +0300

    guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
+var v=true;
+var i=0;
 class GuessingGame {
     constructor() {}
 
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = Math.round((this.min + this.max) / 2);
+        return this.value;
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
 }
+
+module.exports = GuessingGame;
 
 module.exports = GuessingGame;
